7dc086791fb9df3790f2aa2a86d0447f7c589cd1
Liz <Liz@popos>
2024-10-09 12:37:03 -0400

    Reduced dramatic pause, added continue option

--- wordguess.py
+++ wordguess.py
@@ -13,6 +13,13 @@ signal.signal(signal.SIGINT, signal_handler)
 
 wd=os.path.dirname(os.path.realpath(__file__))
 
+difficulty={
+  1: "easiest",
+  2: "medium",
+  3: "harder",
+  4: "hardest"
+}
+
 #-- intro
 print()
 print("                  Welcome to Word Guess!")
@@ -54,7 +61,7 @@ print()
 print("--- You chose the %s wordlist ---" %(wlname))
 print()
 print()
-time.sleep(2)
+time.sleep(1)
 
 #-- choose difficulty
 diff=""
@@ -67,119 +74,149 @@ while not diff.isnumeric() or int(diff) not in range(1,5):
   print("Choose a difficulty from 1 to 4, 4 being the hardest and revealing no letters:")
   diff=getch.getch()
 
+diff=int(diff)
 print()
 print()
 
-# difficulty settings: less letter hints and longer words for greater difficulty
-if int(diff)==1:
-  words=list(filter(lambda item: len(item) < 8, words))
-  word=random.choice(words)
-  missing=random.choices(word, k=int(len(word)/2))
+print("--- You chose the %s difficulty ---" %(difficulty[diff]))
+
+'''if int(diff)==1:
   print("--- You chose the easiest difficulty ---")
 
 elif int(diff)==2:
-  words=list(filter(lambda item: len(item) > 4, words))
-  word=random.choice(words)
-  missing=random.choices(word, k=int(int(len(word)/2)+int(len(word)/2)/3))
   print("--- You chose the medium difficulty ---")
 
 elif int(diff)==3:
-  words=list(filter(lambda item: len(item) > 5, words))
-  word=random.choice(words)
-  missing=random.choices(word, k=int(int(len(word)/2)+int(len(word)/2)/2))
   print("--- You chose the harder difficulty ---")
 
 elif int(diff)==4:
-  word=random.choice(words)
-  missing=word
-  print("--- You chose the hardest difficulty ---")
+  print("--- You chose the hardest difficulty ---")'''
 
-print()
-print()
-time.sleep(2)
+playagain="y"
 
-missing=list(set(missing))
+def gameloop():
+  global playagain
+  global words
+  global guesses
+  global maxguesses
+  global oword
 
-# save the original word, then make a list of the word's letters to parse
-oword=word
-word=list(word)
+  while playagain=="y" or playagain=="Y":
+    # difficulty settings: less letter hints and longer words for greater difficulty
+    if int(diff)==1:
+      words=list(filter(lambda item: len(item) < 8, words))
+      word=random.choice(words)
+      missing=random.choices(word, k=int(len(word)/2))
 
-# no guesses less than 2, and no more than 6, not sure what I was doing with the string formatting...
-maxguesses=int(len(oword)/2)-int(diff)+2
-if int(diff)==4:
-  maxguesses=int("{mg}".format(mg=5 if int(len(oword)/3)+2 < 5 else int(len(oword)/3)+int(len(oword)/3)))
-  maxguesses=6
-elif maxguesses<2:
-  maxguesses=2
-elif maxguesses>6:
-  maxguesses=6
+    elif int(diff)==2:
+      words=list(filter(lambda item: len(item) > 4, words))
+      word=random.choice(words)
+      missing=random.choices(word, k=int(int(len(word)/2)+int(len(word)/2)/3))
 
-# make a list of guessed letters
-guessedlist=[]
-for idx in range(0, maxguesses):
-  guessedlist.append("_")
+    elif int(diff)==3:
+      words=list(filter(lambda item: len(item) > 5, words))
+      word=random.choice(words)
+      missing=random.choices(word, k=int(int(len(word)/2)+int(len(word)/2)/2))
 
-nword="_".ljust(len(word), "_")
-nword=list(nword)
-
-for idx, letter in enumerate(word):
-  if letter in missing:
-    word[idx]="_"
-    nword[idx]=letter
-
-thing=0
-guess="start"
-guesses=0
-guessed=""
-
-while guesses<maxguesses and ''.join(word)!=oword:
-  if guess.isalpha() or "start" in guess:
-    showguessed=""
-    print("=".ljust(20, "="))
-    print(wlname.center(20, " "))
-    print()
-    print()
-    print(" ", ' '.join(word))
-    print()
-    print()
-    print("Misses:", " ".join(guessedlist))
-    print()
-    print("Which letter do you guess? ")
-    print()
-
-  guess=getch.getch()
-  guess=guess.upper()
-
-  if guess.isalpha() or "start" in guess:
-    print(guess)
+    elif int(diff)==4:
+      word=random.choice(words)
+      missing=word
 
     print()
+    print()
+    time.sleep(1)
 
-    if guess in missing and guess not in word:
-      for idx, letter in enumerate(nword):
-        if letter==guess:
-          nword[idx]=""
-          word[idx]=letter
-      print("It's there!")
-    elif guess in guessed:
-      print("You already tried that!")
-    elif guess in word:
-      print("It's already there!")
+    missing=list(set(missing))
+
+    # save the original word, then make a list of the word's letters to parse
+    oword=word
+    word=list(word)
+
+    # no guesses less than 2, and no more than 6, not sure what I was doing with the string formatting...
+    maxguesses=int(len(oword)/2)-int(diff)+2
+    if int(diff)==4:
+      maxguesses=int("{mg}".format(mg=5 if int(len(oword)/3)+2 < 5 else int(len(oword)/3)+int(len(oword)/3)))
+      maxguesses=6
+    elif maxguesses<2:
+      maxguesses=2
+    elif maxguesses>6:
+      maxguesses=6
+
+    # make a list of guessed letters
+    guessedlist=[]
+    for idx in range(0, maxguesses):
+      guessedlist.append("_")
+
+    nword="_".ljust(len(word), "_")
+    nword=list(nword)
+
+    for idx, letter in enumerate(word):
+      if letter in missing:
+        word[idx]="_"
+        nword[idx]=letter
+
+    thing=0
+    guess="start"
+    guesses=0
+    guessed=""
+
+    while guesses<maxguesses and ''.join(word)!=oword:
+      if guess.isalpha() or "start" in guess:
+        showguessed=""
+        print("=".ljust(20, "="))
+        print(wlname.center(20, " "))
+        print()
+        print()
+        print(" ", ' '.join(word))
+        print()
+        print()
+        print("Misses:", " ".join(guessedlist))
+        print()
+        print("Which letter do you guess? ")
+        print()
+
+      guess=getch.getch()
+      guess=guess.upper()
+
+      if guess.isalpha() or "start" in guess:
+        print(guess)
+
+        print()
+
+        if guess in missing and guess not in word:
+          for idx, letter in enumerate(nword):
+            if letter==guess:
+              nword[idx]=""
+              word[idx]=letter
+          print("It's there!")
+        elif guess in guessed:
+          print("You already tried that!")
+        elif guess in word:
+          print("It's already there!")
+        else:
+          guesses=guesses+1
+          guessed=guessed+" "+guess
+          guessedlist[guesses-1]=guess
+          print("Unfortunately, %s is not there." %(guess))
+
+        print()
+        print()
+        time.sleep(1)
+
+    print()
+
+    if guesses < maxguesses:
+      print("".join(["*** It was ", oword, ", you got it!! ***"]))
     else:
-      guesses=guesses+1
-      guessed=guessed+" "+guess
-      guessedlist[guesses-1]=guess
-      print("Unfortunately, %s is not there." %(guess))
+      print("Nice try, it was", oword)
 
     print()
     print()
-    time.sleep(2)
 
-print()
+    print("Continue with %s on %s difficulty? (Y/N)" %(wlname, difficulty[diff]))
+    playagain=getch.getch()
 
-if guesses < maxguesses:
-  print("".join(["*** It was ", oword, ", you got it!! ***"]))
-else:
-  print("Nice try, it was", oword)
+gameloop()
+
 
 print()
